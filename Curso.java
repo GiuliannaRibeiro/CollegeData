3033acb630a9c5d3d7e70e57fffa7a5f29c496da
Giulianna Ribeiro <82107688+GiuliannaRibeiro@users.noreply.github.com>
2022-08-24 22:05:51 -0300

    Update Curso.java

--- Curso.java
+++ Curso.java
@@ -3,24 +3,24 @@ package aula2;
 public class Curso {
 	public int codCurso;
 	public String descCurso;
-	public int duracaoSemestre; //"public" é porque estes dados serão publicos. Mas não é uma boa pratica expor os dados
+	public int duracaoSemestre; 
 	
 	//construtor com passagem de dados
 	public Curso(int codCurso, String descCurso, int duracaoSemestre) {
 		
 		this.codCurso = codCurso;
 		this.descCurso = descCurso;
-		this.duracaoSemestre = duracaoSemestre; //"this" é uma referencia a um objeto.
+		this.duracaoSemestre = duracaoSemestre; 
 	}
-	//construtor default (sem passagem de dados)
+	//construtor default 
 	public Curso() {
 		
 	}
 	
-	public void imprimeCurso() { //"void" a função não coloca valor = não tem return.
-		System.out.println("Códico do curso: " + this.codCurso);
-		System.out.println("Descrição do curso: " + this.descCurso);
-		System.out.println("Duração do curso: " + this.duracaoSemestre);
+	public void imprimeCurso() { 
+		System.out.println("CÃ³dico do curso: " + this.codCurso);
+		System.out.println("DescriÃ§Ã£o do curso: " + this.descCurso);
+		System.out.println("DuraÃ§Ã£o do curso: " + this.duracaoSemestre);
 	}
 	
 	
